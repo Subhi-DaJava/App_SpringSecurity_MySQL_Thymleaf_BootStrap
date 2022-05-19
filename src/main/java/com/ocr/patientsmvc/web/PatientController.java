0f3c4c827024dc7e9215348d61bc172c9f651137
Subhi YD <subhy809fr@gmail.com>
2022-05-20 00:58:44 +0200

    v2.0.0 rajout de MySQL, Page.getContent() (pagination)

--- src/main/java/com/ocr/patientsmvc/web/PatientController.java
+++ src/main/java/com/ocr/patientsmvc/web/PatientController.java
@@ -2,9 +2,12 @@ package com.ocr.patientsmvc.web;
 
 import com.ocr.patientsmvc.model.Patient;
 import com.ocr.patientsmvc.repository.PatientRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -18,9 +21,11 @@ public class PatientController {
     }
 
     @GetMapping(path = "/index")
-    public String patients(Model model){
-        List<Patient> patients = patientRepository.findAll();
-        model.addAttribute("listPatients",patients);
+    public String patients(Model model,
+                           @RequestParam(name = "page",defaultValue = "0") int page,
+                           @RequestParam(name = "size", defaultValue = "5") int size){
+        Page<Patient> pagePatients = patientRepository.findAll(PageRequest.of(page,size));
+        model.addAttribute("listPatients",pagePatients.getContent());
         return "patients";
     }
 
