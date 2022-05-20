4d5ed84b690bc5b44c9a762030ef1d44dcab88f5
Subhi YD <subhy809fr@gmail.com>
2022-05-20 18:24:19 +0200

    v3.0.0 rajout de formulaire (pour rechercher), keyword, gardé keyword en navigation

--- src/main/java/com/ocr/patientsmvc/repository/PatientRepository.java
+++ src/main/java/com/ocr/patientsmvc/repository/PatientRepository.java
@@ -1,7 +1,11 @@
 package com.ocr.patientsmvc.repository;
 
 import com.ocr.patientsmvc.model.Patient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+
+    Page<Patient> findByNomContains(String kw, Pageable pageable);
 }
