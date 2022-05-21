3867691bbf0efb6a192b7437da8e4cc9c412c6da
Subhi YD <subhy809fr@gmail.com>
2022-05-21 22:27:10 +0200

    v7.0.1 Authentification avec JDBC, trois tables(users, roles et users_roles) saisir mot de passe en BCrypte, obligé d'utiliser WebSecurityConfigurerAdapter pour mettre en place l'authentification

--- src/main/java/com/ocr/patientsmvc/security/SecurityConfig.java
+++ src/main/java/com/ocr/patientsmvc/security/SecurityConfig.java
@@ -1,14 +1,26 @@
 package com.ocr.patientsmvc.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+import javax.xml.ws.soap.Addressing;
 
 /*
 https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter
@@ -16,9 +28,10 @@ Spring Security without the WebSecurityConfigurerAdapter
  */
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
-    //Pour tester l'application
-    @Bean
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    //In-Memory Authentication pour tester l'application utiliser la mémoire
+  /*  @Bean
     public InMemoryUserDetailsManager userDetailsService(){
 
         PasswordEncoder passwordEncoder = passwordEncoder();
@@ -39,12 +52,43 @@ public class SecurityConfig {
                 .password(passwordEncoder.encode("91011"))   //.password("{noop}1234") éviter à encoder (sans passwordEncoder)
                 .roles("USER","ADMIN")
                 .build();
+        //avoir le Bcrypt
+        //System.out.println(passwordEncoder.encode("1234"));
+
         return new InMemoryUserDetailsManager(user,user2,admin);
+    }*/
+    // https://www.baeldung.com/spring-security-jdbc-authentication
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        PasswordEncoder passwordEncoder = passwordEncoder();
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select username as principal, password as credentials, active from users where username=?")
+                .authoritiesByUsernameQuery("select username as principal, role as role from users_roles where username=?")
+                .rolePrefix("ROLE_")
+                .passwordEncoder(passwordEncoder);
+
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.formLogin();
+        //Autoriser les users pour accéder à des pages
+        http.authorizeHttpRequests().antMatchers("/").permitAll(); //ça ne nécessite pas à une authentification
+        http.authorizeHttpRequests().antMatchers("/admin/**").hasRole("ADMIN");
+        http.authorizeHttpRequests().antMatchers("/user/**").hasRole("USER");
+
+        //Autoriser les ressources statics
+        http.authorizeHttpRequests().antMatchers("/webjars/**").permitAll();
+
+        http.authorizeHttpRequests().anyRequest().authenticated();
+        http.exceptionHandling().accessDeniedPage("/403");
     }
 
 
-
-    @Bean
+  /*  @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.formLogin();
         //Autoriser les users pour accéder à des pages
@@ -54,7 +98,8 @@ public class SecurityConfig {
         http.authorizeHttpRequests().anyRequest().authenticated();
         http.exceptionHandling().accessDeniedPage("/403");
         return http.build();
-    }
+    }*/
+
     @Bean
     PasswordEncoder passwordEncoder(){
 
